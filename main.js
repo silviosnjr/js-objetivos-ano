4a79cd96c16c650cbb669ecacafefdbf7f355f68
silviosnjr <silviosnjr@gmail.com>
2024-12-04 08:25:27 -0300

    código fonte da aula 6

--- main.js
+++ main.js
@@ -16,8 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-05-26T00:00:00");
+const tempoObjetivo2 = new Date("2025-07-24T00:00:00");
+const tempoObjetivo3 = new Date("2025-08-20T00:00:00");
+const tempoObjetivo4 = new Date("2025-09-22T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -30,6 +34,22 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
